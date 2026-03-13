3a57371a6fffa95a07e1913c85c9bc8f7d19b415
ikartikeyo <kartikeypandey0011@gmail.com>
2026-03-13 18:30:47 +0530

    yupp

--- server.js
+++ server.js
@@ -1,5 +1,14 @@
 const app = require('./app');
 const PORT = process.env.PORT || 5000;
+// Add this at the very top of server.js
+process.on('unhandledRejection', (reason, promise) => {
+  console.error('Unhandled Rejection at:', promise, 'reason:', reason);
+});
+
+process.on('uncaughtException', (error) => {
+  console.error('Uncaught Exception:', error);
+  process.exit(1);
+});
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
